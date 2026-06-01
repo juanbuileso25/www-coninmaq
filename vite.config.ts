2053b4bd1624572eb2fbac7ad1ad94b359bf3c3f
Juan David Builes Ochoa <j.builes@visiontravelmail.net>
2026-06-01 09:53:33 -0500

    [CHANGE] Base url

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
-  base: "/www-coninmaq/",
+    plugins: [react()],
+    base: "/",
 });
